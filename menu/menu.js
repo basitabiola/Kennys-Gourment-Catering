b538dd874cc602af8391c068f829d26dfb01f6f6
BASIT ABIOLA <154072273+basitabiola@users.noreply.github.com>
2025-11-11 21:48:02 +0100

    Add files via upload

---
+++ menu/menu.js
@@ -0,0 +1,28 @@
+
+
+
+let fsticky = document.getElementById("fsticky").innerHTML;
+let wsticky = document.getElementById("wsticky").innerHTML;
+let gsticky = document.getElementById("gsticky").innerHTML;
+
+let social = document.getElementById("sticky");
+
+social.addEventListener("click", function () {
+  social.style.display = "none";
+
+  let blur = document.getElementById("blurs");
+  blur.style.display = "block";
+});
+
+blurs.addEventListener("click", function () {
+  let blurs = document.getElementById("blurs");
+  blurs.style.display = "none";
+  social.style.display = "block";
+  let cancel = document.getElementById("cancel").innerHTML;
+  cancel.style.display = "block";
+});
+
+
+
+
+
